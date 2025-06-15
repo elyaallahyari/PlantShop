af6721f8ff5546043a3fbdb3eb23dc009822d36d
Elya <elyaallahyari@gmail.com>
2025-06-15 13:41:35 +0330

    Edit image sources

--- src/components/CustomerReview.jsx
+++ src/components/CustomerReview.jsx
@@ -1,9 +1,6 @@
 import '../assets/styles/CustomerReview.css'
 import titleIcon from '../assets/images/CustomerReview.png'
-import reviewIcon1 from '../assets/images/review1.png'
-import reviewIcon2 from '../assets/images/review2.png'
-import reviewIcon3 from '../assets/images/review3.png'
-
+import reviewBackground from '../assets/images/newsBac.svg'
 function CustomerReview(){
     return(
         <>
@@ -12,9 +9,9 @@ function CustomerReview(){
                 <img src={titleIcon}/>
             </div>
             <div className="CustomerReview__items">
-                <img src={reviewIcon1}/>
-                <img src={reviewIcon2}/>
-                <img src={reviewIcon3}/>
+                <img src={reviewBackground}/>
+                <img src={reviewBackground}/>
+                <img src={reviewBackground}/>
 
 
             </div>
--- src/components/TopSelling.jsx
+++ src/components/TopSelling.jsx
@@ -1,12 +1,6 @@
 import '../assets/styles/TopSelling.css'
 import topSellingIcon from '../assets/images/topSelling.png'
-import productIcon1 from '../assets/images/plant1.png'
-import productIcon2 from '../assets/images/plant2.png'
-import productIcon3 from '../assets/images/plant3.png'
-import productIcon4 from '../assets/images/plant4.png'
-import productIcon5 from '../assets/images/plant5.png'
-import productIcon6 from '../assets/images/plant6.png'
-
+import productBackground from '../assets/images/plantBac.svg'
 function TopSelling() {
   return (
     <>
@@ -15,12 +9,12 @@ function TopSelling() {
           <img src={topSellingIcon} alt="topSellingIcon" id="plants" />
         </div>
         <div className="topSelling__products">
-            <img src={productIcon1} />
-            <img src={productIcon2} />
-            <img src={productIcon3} />
-            <img src={productIcon4} />
-            <img src={productIcon5} />
-            <img src={productIcon6} />
+            <img src={productBackground} />
+            <img src={productBackground} />
+            <img src={productBackground} />
+            <img src={productBackground} />
+            <img src={productBackground} />
+            <img src={productBackground} />
 
         </div>
       </div>
